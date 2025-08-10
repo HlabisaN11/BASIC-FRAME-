a247cfd759101b78050c6df380848cc9035eed3c
HlabisaN11 <nkululekohlabisa7@gmail.com>
2025-08-10 21:19:51 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+
+import za.ac.tut.ui.myFrame;
+
+/**
+ *
+ * @author nkulu
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        myFrame myframe;
+        
+        myframe = new myFrame();
+    }
+    
+}
